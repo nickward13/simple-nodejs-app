1ee76180330f1c8e1892664e237b86d36d70e6be
Nick Ward <nickward@microsoft.com>
2018-06-14 11:16:29 +1000

    added IP address and hostname

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+var os = require('os');
+var dns = require('dns');
 
 // Constants
 const PORT = 8080;
@@ -9,7 +11,13 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world!\n');
+  dns.lookup(os.hostname(), function (err, add, fam) {
+    var message = '<h1>Hello world!</h1>';
+    message = message + ('<p>Hostname: ' + os.hostname());
+    message = message + ('<p>OS Platform: ' + os.platform());
+    message = message + ('<p>IP Address: ' + add + '\n');
+    res.send(message);
+  })
 });
 
 app.listen(PORT, HOST);
