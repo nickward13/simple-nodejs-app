f640f25fad352390378d0d0eb0b8582b03e080d9
Nick Ward <nickward@microsoft.com>
2018-07-09 12:34:30 +1000

    simplified host address lookup

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ var dns = require('dns');
 const appInsights = require('applicationinsights');
 appInsights.setup().start();
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
 const app = express();
 
-app.get('/', (req, res) => {
-  dns.lookup(os.hostname(), function (err, add, fam) {
-    var message = '<h1>Hello world!</h1>';
-    message = message + ('<p>Hostname: ' + os.hostname());
-    message = message + ('<p>OS Platform: ' + os.platform());
-    message = message + ('<p>IP Address: ' + add + '\n');
-    res.send(message);
-  })
+const PORT = 8080;
+
+var hostAddress = "";
+dns.lookup(os.hostname(), function(err, address, addressFamily) {
+  hostAddress = address;
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/', (req, res) => {
+  var message = '<h1>Hello world!</h1>';
+  message = message + ('<p>Hostname: ' + os.hostname());
+  message = message + ('<p>OS Platform: ' + os.platform());
+  message = message + ('<p>IP Address: ' + hostAddress + '\n');
+  res.send(message);
+});
+
+app.listen(PORT);
+console.log(`Running on http://${hostAddress}:${PORT}`);
